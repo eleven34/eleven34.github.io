48f6a96e4da13776353c2c660543ac2f6f4e2bfa
Elliott Quinn <geewhatamale+github@gmail.com>
2026-06-10 18:42:47 -0500

    2026 Updates

--- script.js
+++ script.js
@@ -8,7 +8,7 @@ let opentl = anime.timeline({
     autoplay: false,
     duration: 1500
   });
-  
+
 opentl
 .add({
   targets: zoomer,
@@ -20,28 +20,28 @@ opentl
   }
 })
 .add({
-  targets: '#board1',  
+  targets: '#board1',
   keyframes: [
     {translateY: ['-100%', 0]},
   ],
   zIndex: 4
 }, '-=1500')
 .add({
-  targets: '#board2',  
+  targets: '#board2',
   keyframes: [
     {translateY: ['-100%', 0], rotateX: [-180, 0]},
   ],
   zIndex: 3
 }, '-=1500')
 .add({
-  targets: '#board3',  
+  targets: '#board3',
   keyframes: [
     {translateY: ['-200%', 0], rotateX: [180, 0]},
   ],
   zIndex: 2
 }, '-=1500')
 .add({
-  targets: '#board4',  
+  targets: '#board4',
   keyframes: [
     {translateY: ['-300%', 0], rotateX: [-180, 0]},
   ],
@@ -55,11 +55,11 @@ opentl
 })
 
 
-animateButt.addEventListener('click', () => {    
+animateButt.addEventListener('click', () => {
   if (!isOpen) {
     opentl.play();
     animateButt.setAttribute('aria-expanded', 'true');
     animateButt.setAttribute('disabled', '');
     isOpen = true;
-  }  
+  }
 });
